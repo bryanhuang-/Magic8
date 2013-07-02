e5c06008fdec5309f5e04554962c6257423505c9
Bryan Huang <bryan.huang@excella.com>
2013-07-02 10:39:01 -0400

    Testing

---
+++ Magic8/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="Magic8._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <p>
+        <asp:Button ID="fortuneButton" runat="server" Text="Click for fortune!" 
+            Width="232px" onclick="fortuneButton_Click" />
+        <asp:TextBox ID="fortuneTextBox" runat="server" Width="438px" ReadOnly="True"></asp:TextBox>
+    </p>
+<p>
+        <asp:Label ID="countLabel" runat="server" Text="Fortunes given: 0"></asp:Label>
+    </p>
+    <p>
+        Created by Bryan Huang, Copyright 2013</p>
+</asp:Content>
---
+++ Magic8/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Magic8
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void fortuneButton_Click(object sender, EventArgs e)
+        {
+            FortuneGenerator.generateFortune(fortuneTextBox, countLabel);
+        }
+    }
+
+    public class FortuneGenerator
+    {
+        static string[] fortunes = {
+                                "It is certain",
+                                "It is decidedly so",
+                                "Without a doubt",
+                                "Yes definitely",
+                                "You may rely on it",
+                                "As I see it yes",
+                                "Most likely",
+                                "Outlook good",
+                                "Yes",
+                                "Signs point to yes",
+                                "Reply hazy try again",
+                                "Ask again later",
+                                "Better not tell you now",
+                                "Cannot predict now",
+                                "Concentrate and ask again",
+                                "Don't count on it",
+                                "My reply is no",
+                                "My sources say no",
+                                "Outlook not so good",
+                                "Very doubtful"};
+        static Random generator;
+        static int fortuneCount;
+
+        public static void generateFortune(TextBox fortuneTextBox, Label countLabel)
+        {
+            generator = new Random();
+            int fortune = generator.Next(0, 20);
+            fortuneTextBox.Text = fortunes[fortune].ToString();
+            fortuneCount++;
+            countLabel.Text = String.Format("Fortunes given: {0}", fortuneCount);
+        }
+    }
+}
